852e847881e065ba3723b499b80ce732fcee2ff7
Lian Granot <lian@sternumiot.com>
2024-01-02 08:56:13 +0200

    added a comment

--- example_build_config.mk
+++ example_build_config.mk
@@ -2,6 +2,7 @@ OUTPUT_FILE = dummy.a
 # Header files that will be copied to the build dir
 EXTERNAL_HEADERS_SRC = $(HEADER_DIRS)/dummy.h
 
+# Define TOOLCHAIN to make sure your unitests will be compiled exactly as your target archive
 ifdef TOOLCHAIN
 CC=$(TOOLCHAIN)-gcc
 AR=$(TOOLCHAIN)-ar
